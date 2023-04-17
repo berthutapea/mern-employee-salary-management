ff1285675193e2ea846ee28633866ae56abd89f0
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-04-18 05:13:09 +0700

    Refactor

--- Frontend/src/pages/MasterData/DataPegawai/DataPegawai.jsx
+++ Frontend/src/pages/MasterData/DataPegawai/DataPegawai.jsx
@@ -1,24 +1,54 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import DefaultLayout from '../../../layout/DefaultLayout';
-import User1 from '../../../Assets/images/user/gilbert.png'
-import User2 from '../../../Assets/images/user/user-02.png'
-import User3 from '../../../Assets/images/user/user-01.png'
-import User4 from '../../../Assets/images/user/user-04.png'
+import DataPegawaiPeople from '../../../Utils/DataPegawaiPeople';
+
 import Breadcrumb from '../../../components/Breadcrumb/Breadcrumb';
 import { FaRegEdit } from 'react-icons/fa'
 import { BsTrash3 } from 'react-icons/bs'
 import { BiSearch } from 'react-icons/bi'
 import { MdOutlineKeyboardArrowDown } from 'react-icons/md'
 
-
+const ITEMS_PER_PAGE = 4;
 
 const DataPegawai = () => {
+    const [currentPage, setCurrentPage] = useState(1);
+    const [startIndex, setStartIndex] = useState(0);
+    const [endIndex, setEndIndex] = useState(ITEMS_PER_PAGE);
+    const [dataPegawai, setDataPegawai] = useState([]);
+
+    const totalPages = Math.ceil(DataPegawaiPeople.length / ITEMS_PER_PAGE);
+
+    useEffect(() => {
+        setDataPegawai(DataPegawaiPeople.slice(startIndex, endIndex));
+    }, [startIndex, endIndex]);
+
+    const goToPrevPage = () => {
+        if (currentPage > 1) {
+            setCurrentPage((prev) => prev - 1);
+            setStartIndex((prev) => prev - ITEMS_PER_PAGE);
+            setEndIndex((prev) => prev - ITEMS_PER_PAGE);
+        }
+    };
+
+    const shouldShowEllipsis = (startIndex, endIndex) => {
+        return endIndex !== totalPages && endIndex - startIndex !== 2;
+    };
+
+    const goToNextPage = () => {
+        if (currentPage < totalPages) {
+            setCurrentPage((prev) => prev + 1);
+            setStartIndex((prev) => prev + ITEMS_PER_PAGE);
+            setEndIndex((prev) => prev + ITEMS_PER_PAGE);
+        }
+    };
+
+
     return (
         <DefaultLayout>
             <Breadcrumb pageName='Data Pegawai' />
 
             <div className='rounded-sm border border-stroke bg-white px-5 pt-6 pb-2.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1'>
-                <div className="flex flex-col md:flex-row md:justify-between">
+                <div className="flex justify-between items-center mt-4 flex-col md:flex-row md:justify-between">
                     <div className="relative flex-1 md:mr-2 mb-4 md:mb-0 ">
                         <div className='relative'>
                             <span className='absolute top-1/2 left-48 z-30 -translate-y-1/2 text-xl'>
@@ -71,140 +101,114 @@ const DataPegawai = () => {
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td className='border-b border-[#eee] py-5 px-4 pl-9 dark:border-strokedark'>
-                                    <div className="h-12.5 w-15">
-                                        <img className='rounded-full' src={User1} alt="Gilbert Hutapea" />
-                                    </div>
-                                </td>
-                                <td className='border-b border-[#eee] py-5 px-4 dark:border-strokedark'>
-                                    <p className='text-black dark:text-white'>1218052110020002</p>
-                                </td>
-                                <td className='border-b border-[#eee] py-5 px-4 dark:border-strokedark'>
-                                    <p className='text-black dark:text-white'>Gilbert Hutapea</p>
-                                </td>
-                                <td className='border-b border-[#eee] py-5 px-4 dark:border-strokedark'>
-                                    <p className='text-black dark:text-white'>Laki-Laki</p>
-                                </td>
-                                <td className='border-b border-[#eee] py-5 px-4 dark:border-strokedark'>
-                                    <p className='text-black dark:text-white'>15-01-2023</p>
-                                </td>
-                                <td className='border-b border-[#eee] py-5 px-4 dark:border-strokedark'>
-                                    <p className='text-black dark:text-white'>Karyawan Tetap</p>
-                                </td>
-                                <td className='border-b border-[#eee] py-5 px-4 dark:border-strokedark'>
-                                    <div className='flex items-center space-x-3.5'>
-                                        <button className='hover:text-black'>
-                                            <FaRegEdit className="text-primary text-xl hover:text-black dark:hover:text-white" />
-                                        </button>
-                                        <button className='hover:text-black'>
-                                            <BsTrash3 className="text-danger text-xl hover:text-black dark:hover:text-white" />
-                                        </button>
-                                    </div>
-                                </td>
-                            </tr>
-
-                            <tr>
-                                <td className='border-b border-[#eee] py-5 px-4 pl-9 dark:border-strokedark'>
-                                    <div className="h-12.5 w-15">
-                                        <img className='rounded-full' src={User2} alt="Layla Siregar" />
-                                    </div>
-                                </td>
-                                <td className='border-b border-[#eee] py-5 px-4 dark:border-strokedark'>
-                                    <p className='text-black dark:text-white'>1218052110020003</p>
-                                </td>
-                                <td className='border-b border-[#eee] py-5 px-4 dark:border-strokedark'>
-                                    <p className='text-black dark:text-white'>Layla Siregar</p>
-                                </td>
-                                <td className='border-b border-[#eee] py-5 px-4 dark:border-strokedark'>
-                                    <p className='text-black dark:text-white'>Perempuan</p>
-                                </td>
-                                <td className='border-b border-[#eee] py-5 px-4 dark:border-strokedark'>
-                                    <p className='text-black dark:text-white'>16-02-2023</p>
-                                </td>
-                                <td className='border-b border-[#eee] py-5 px-4 dark:border-strokedark'>
-                                    <p className='text-black dark:text-white'>Karyawan Tidak Tetap</p>
-                                </td>
-                                <td className='border-b border-[#eee] py-5 px-4 dark:border-strokedark'>
-                                    <div className='flex items-center space-x-3.5'>
-                                        <button className='hover:text-black'>
-                                            <FaRegEdit className="text-primary text-xl hover:text-black dark:hover:text-white" />
-                                        </button>
-                                        <button className='hover:text-black'>
-                                            <BsTrash3 className="text-danger text-xl hover:text-black dark:hover:text-white" />
-                                        </button>
-                                    </div>
-                                </td>
-                            </tr>
-
-                            <tr>
-                                <td className='border-b border-[#eee] py-5 px-4 pl-9 dark:border-strokedark'>
-                                    <div className="h-12.5 w-15">
-                                        <img className='rounded-full' src={User3} alt="Zilong Sibarani" />
-                                    </div>
-                                </td>
-                                <td className='border-b border-[#eee] py-5 px-4 dark:border-strokedark'>
-                                    <p className='text-black dark:text-white'>1218052110020004</p>
-                                </td>
-                                <td className='border-b border-[#eee] py-5 px-4 dark:border-strokedark'>
-                                    <p className='text-black dark:text-white'>Zilong Sibarani</p>
-                                </td>
-                                <td className='border-b border-[#eee] py-5 px-4 dark:border-strokedark'>
-                                    <p className='text-black dark:text-white'>Laki-Laki</p>
-                                </td>
-                                <td className='border-b border-[#eee] py-5 px-4 dark:border-strokedark'>
-                                    <p className='text-black dark:text-white'>17-03-2023</p>
-                                </td>
-                                <td className='border-b border-[#eee] py-5 px-4 dark:border-strokedark'>
-                                    <p className='text-black dark:text-white'>Karyawan Tetap</p>
-                                </td>
-                                <td className='border-b border-[#eee] py-5 px-4 dark:border-strokedark'>
-                                    <div className='flex items-center space-x-3.5'>
-                                        <button className='hover:text-black'>
-                                            <FaRegEdit className="text-primary text-xl hover:text-black dark:hover:text-white" />
-                                        </button>
-                                        <button className='hover:text-black'>
-                                            <BsTrash3 className="text-danger text-xl hover:text-black dark:hover:text-white" />
-                                        </button>
-                                    </div>
-                                </td>
-                            </tr>
-
-                            <tr>
-                                <td className='border-b border-[#eee] py-5 px-4 pl-9 dark:border-strokedark'>
-                                    <div className="h-12.5 w-15">
-                                        <img className='rounded-full' src={User4} alt="Nana Silaban" />
-                                    </div>
-                                </td>
-                                <td className='border-b border-[#eee] py-5 px-4 dark:border-strokedark'>
-                                    <p className='text-black dark:text-white'>1218052110020005</p>
-                                </td>
-                                <td className='border-b border-[#eee] py-5 px-4 dark:border-strokedark'>
-                                    <p className='text-black dark:text-white'>Nana Silaban</p>
-                                </td>
-                                <td className='border-b border-[#eee] py-5 px-4 dark:border-strokedark'>
-                                    <p className='text-black dark:text-white'>Perempuan</p>
-                                </td>
-                                <td className='border-b border-[#eee] py-5 px-4 dark:border-strokedark'>
-                                    <p className='text-black dark:text-white'>18-04-2023</p>
-                                </td>
-                                <td className='border-b border-[#eee] py-5 px-4 dark:border-strokedark'>
-                                    <p className='text-black dark:text-white'>Karyawan Tidak Tetap</p>
-                                </td>
-                                <td className='border-b border-[#eee] py-5 px-4 dark:border-strokedark'>
-                                    <div className='flex items-center space-x-3.5'>
-                                        <button className='hover:text-black'>
-                                            <FaRegEdit className="text-primary text-xl hover:text-black dark:hover:text-white" />
-                                        </button>
-                                        <button className='hover:text-black'>
-                                            <BsTrash3 className="text-danger text-xl hover:text-black dark:hover:text-white" />
-                                        </button>
-                                    </div>
-                                </td>
-                            </tr>
+                            {dataPegawai.map(({ _id, photo, nik, namaPegawai, jenisKelamin, tanggalMasuk, status }) => (
+                                <tr key={dataPegawai.id}>
+                                    <td className='border-b border-[#eee] py-5 px-4 pl-9 dark:border-strokedark'>
+                                        <div className="h-12.5 w-15">
+                                            <div className='rounded-full overflow-hidden'> {photo}</div>
+                                        </div>
+                                    </td>
+                                    <td className='border-b border-[#eee] py-5 px-4 dark:border-strokedark'>
+                                        <p className='text-black dark:text-white'>{nik}</p>
+                                    </td>
+                                    <td className='border-b border-[#eee] py-5 px-4 dark:border-strokedark'>
+                                        <p className='text-black dark:text-white'>{namaPegawai}</p>
+                                    </td>
+                                    <td className='border-b border-[#eee] py-5 px-4 dark:border-strokedark'>
+                                        <p className='text-black dark:text-white'>{jenisKelamin}</p>
+                                    </td>
+                                    <td className='border-b border-[#eee] py-5 px-4 dark:border-strokedark'>
+                                        <p className='text-black dark:text-white'>{tanggalMasuk}</p>
+                                    </td>
+                                    <td className='border-b border-[#eee] py-5 px-4 dark:border-strokedark'>
+                                        <p className='text-black dark:text-white'>{status}</p>
+                                    </td>
+                                    <td className='border-b border-[#eee] py-5 px-4 dark:border-strokedark'>
+                                        <div className='flex items-center space-x-3.5'>
+                                            <button className='hover:text-black'>
+                                                <FaRegEdit className="text-primary text-xl hover:text-black dark:hover:text-white" />
+                                            </button>
+                                            <button className='hover:text-black'>
+                                                <BsTrash3 className="text-danger text-xl hover:text-black dark:hover:text-white" />
+                                            </button>
+                                        </div>
+                                    </td>
+                                </tr>
+                            ))}
                         </tbody>
                     </table>
                 </div>
+
+                    <div className='flex justify-between items-center mt-4 flex-col md:flex-row md:justify-between'>
+                        <div className='flex items-center space-x-2'>
+                            <span className='text-gray-5 dark:text-gray-4 text-sm py-4'>
+                                Showing {startIndex}-{endIndex} of {DataPegawaiPeople.length} Data Pegawai
+                            </span>
+                        </div>
+                        <div className='flex space-x-2 py-4'>
+                            <button
+                                disabled={currentPage === 1}
+                                onClick={goToPrevPage}
+                                className='py-2 px-6 rounded-lg border border-primary text-primary font-semibold hover:bg-primary hover:text-white dark:text-white dark:border-primary dark:hover:bg-primary dark:hover:text-white disabled:opacity-50'
+                            >
+                                Prev
+                            </button>
+                            {[...Array(Math.min(totalPages, 5))].map((_, i) => {
+                                const page = i + 1;
+                                if (page === currentPage) {
+                                    return (
+                                        <div
+                                            key={i}
+                                            className="py-2 px-4 rounded-lg border border-primary bg-primary text-white font-semibold hover:bg-primary dark:text-white dark:bg-primary dark:hover:bg-primary"
+                                        >
+                                            {page}
+                                        </div>
+                                    );
+                                } else if (page === 2 && currentPage > 4) {
+                                    return (
+                                        <p
+                                            key={i}
+                                            className="py-2 px-4 border border-gray-2 dark:bg-transparent text-black font-medium bg-gray dark:border-strokedark dark:text-white"
+                                        >
+                                            ...
+                                        </p>
+                                    );
+                                } else if (page === totalPages - 1 && currentPage < totalPages - 3) {
+                                    return (
+                                        <p
+                                            key={i}
+                                            className="py-2 px-4 border border-gray-2 dark:bg-transparent text-black font-medium bg-gray dark:border-strokedark dark:text-white"
+                                        >
+                                            ...
+                                        </p>
+                                    );
+                                } else if (
+                                    page === 1 ||
+                                    page === totalPages ||
+                                    (page >= currentPage - 1 && page <= currentPage + 1)
+                                ) {
+                                    return (
+                                        <div
+                                            key={i}
+                                            className="py-2 px-4 rounded-lg border border-gray-2 text-black dark:bg-transparent bg-gray font-medium dark:border-strokedark dark:text-white"
+                                        >
+                                            {page}
+                                        </div>
+                                    );
+                                } else {
+                                    return null;
+                                }
+                            })}
+
+                            <button
+                                disabled={currentPage === totalPages}
+                                onClick={goToNextPage}
+                                className='py-2 px-6 rounded-lg border border-primary text-primary font-semibold hover:bg-primary hover:text-white dark:text-white dark:border-primary dark:hover:bg-primary dark:hover:text-white disabled:opacity-50'
+                            >
+                                Next
+                            </button>
+                        </div>
+                    </div>
             </div>
         </DefaultLayout>
     )
