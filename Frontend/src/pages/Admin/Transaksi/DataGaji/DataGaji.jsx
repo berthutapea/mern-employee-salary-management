9165214025f2cddb91dbe5d4799299d78734be2f
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-04-21 09:27:03 +0700

    Refactor

--- Frontend/src/pages/Admin/Transaksi/DataGaji/DataGaji.jsx
+++ Frontend/src/pages/Admin/Transaksi/DataGaji/DataGaji.jsx
@@ -1,12 +1,14 @@
 import { useState, useEffect } from 'react';
 import DefaultLayout from '../../../../layout/DefaultLayout';
-import DataJabatanPeople from '../../../../utils/DataJabatanPeople';
+import DataGajiPegawai from '../../../../utils/DataGajiPegawai';
 import { Link } from "react-router-dom";
 import Breadcrumb from '../../../../components/Breadcrumb/Breadcrumb';
 import ButtonOne from '../../../../components/Buttons/ButtonOne/ButtonOne';
-import { FaRegEdit, FaPlus } from 'react-icons/fa'
+import { FaRegEdit } from 'react-icons/fa'
 import { BsTrash3 } from 'react-icons/bs'
 import { BiSearch } from 'react-icons/bi'
+import { MdOutlineKeyboardArrowDown } from 'react-icons/md'
+import { TfiEye, TfiPrinter } from 'react-icons/tfi'
 
 const ITEMS_PER_PAGE = 4;
 
@@ -14,12 +16,12 @@ const DataGaji = () => {
     const [currentPage, setCurrentPage] = useState(1);
     const [startIndex, setStartIndex] = useState(0);
     const [endIndex, setEndIndex] = useState(ITEMS_PER_PAGE);
-    const [dataJabatan, setDataJabatan] = useState([]);
+    const [dataGaji, setDataGaji] = useState([]);
 
-    const totalPages = Math.ceil(DataJabatanPeople.length / ITEMS_PER_PAGE);
+    const totalPages = Math.ceil(DataGajiPegawai.length / ITEMS_PER_PAGE);
 
     useEffect(() => {
-        setDataJabatan(DataJabatanPeople.slice(startIndex, endIndex));
+        setDataGaji(DataGajiPegawai.slice(startIndex, endIndex));
     }, [startIndex, endIndex]);
 
     const goToPrevPage = () => {
@@ -38,18 +40,88 @@ const DataGaji = () => {
         }
     };
 
-
     return (
         <DefaultLayout>
             <Breadcrumb pageName='Data Gaji Pegawai' />
-            <Link to="/admin/master-data/data-jabatan/form-data-jabatan" >
-                <ButtonOne  >
-                    <span>Tambah Jabatan</span>
-                    <span>
-                        <FaPlus />
-                    </span>
-                </ButtonOne>
-            </Link>
+
+            <div className='rounded-sm border border-stroke bg-white px-5 pt-2 pb-2.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-10 mt-6'>
+                <div className='border-b border-stroke py-2 dark:border-strokedark'>
+                    <h3 className='font-medium text-black dark:text-white'>
+                        Filter Data Gaji Pegawai
+                    </h3>
+                </div>
+
+                <div className="flex justify-between items-center mt-4 flex-col md:flex-row md:justify-between">
+                    <div className="relative w-full md:w-1/4 md:mr-2 mb-4 md:mb-0">
+                        <div className='relative'><span className='px-4'> Bulan</span>
+                            <span className='absolute top-1/2 left-55 z-30 -translate-y-1/2 text-xl'>
+                                <MdOutlineKeyboardArrowDown />
+                            </span>
+                            <select className='relative appearance-none rounded border border-stroke bg-transparent py-2 px-12 outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input'>
+                                <option value=''>Pilih Bulan</option>
+                                <option value=''>Januari</option>
+                                <option value=''>Februari</option>
+                                <option value=''>Maret</option>
+                                <option value=''>April</option>
+                                <option value=''>Mei</option>
+                                <option value=''>Juni</option>
+                                <option value=''>Juli</option>
+                                <option value=''>Agustus</option>
+                                <option value=''>September</option>
+                                <option value=''>Oktober</option>
+                                <option value=''>November</option>
+                                <option value=''>Desember</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div className="relative w-full md:w-1/4 md:mr-2 mb-4 md:mb-0">
+                        <div className='relative'><span className='px-4'>Tahun</span>
+                            <span className='absolute top-1/2 left-55 z-30 -translate-y-1/2 text-xl'>
+                                <MdOutlineKeyboardArrowDown />
+                            </span>
+                            <select className='relative appearance-none rounded border border-stroke bg-transparent py-2 px-12 outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input'>
+                                <option value=''>Pilih Tahun</option>
+                                <option value=''>2020</option>
+                                <option value=''>2021</option>
+                                <option value=''>2022</option>
+                                <option value=''>2023</option>
+                                <option value=''>2024</option>
+                                <option value=''>2025</option>
+                                <option value=''>2026</option>
+                                <option value=''>2027</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div className='flex flex-col md:flex-row w-full md:w-1/2 justify-between text-center'>
+                        <div className="relative w-full md:w-1/2 mb-4 md:mb-0 ">
+                            <Link to="/admin/transaksi/data-gaji">
+                                <ButtonOne className="bg-primary">
+                                    <span>Tampilkan Data</span>
+                                    <span>
+                                        <TfiEye />
+                                    </span>
+                                </ButtonOne>
+                            </Link>
+                        </div>
+                        <div className="relative w-full md:w-1/2  mb-4 md:mb-0">
+                            <Link to="/admin/transaksi/data-gaji">
+                                <ButtonOne>
+                                    <span>Cetak Daftar Gaji</span>
+                                    <span>
+                                        <TfiPrinter />
+                                    </span>
+                                </ButtonOne>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="bg-gray-2 text-left dark:bg-meta-4 mt-6">
+                    <h2 className="px-4 py-2 text-black dark:text-white">Menampilkan Data Gaji Pegawai Bulan:
+                        <span className="font-medium"> April</span> Tahun:<span className="font-medium"> 2023</span></h2>
+                </div>
+            </div>
+
             <div className='rounded-sm border border-stroke bg-white px-5 pt-6 pb-2.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1 mt-6'>
                 <div className="flex justify-between items-center mt-4 flex-col md:flex-row md:justify-between">
                     <div className="relative flex-2 mb-4 md:mb-0">
@@ -65,49 +137,73 @@ const DataGaji = () => {
                 </div>
 
                 <div className='max-w-full overflow-x-auto py-4'>
-                    <table className='w-full table-auto'>
+                    <table className='w-full table-auto-full'>
                         <thead>
-                            <tr className='bg-gray-2 text-left dark:bg-meta-4'>
-                                <th className='py-4 px-4 font-medium text-black dark:text-white'>
+                            <tr className='bg-gray-2  dark:bg-meta-4'>
+                                <th className='py-2 px-2 font-medium text-black dark:text-white'>
+                                    NIK
+                                </th>
+                                <th className='py-2 px-2 font-medium text-black dark:text-white'>
+                                    Nama <br /> Pegawai
+                                </th>
+                                <th className='py-2 px-2 font-medium text-black dark:text-white'>
+                                    Jenis <br /> Kelamin
+                                </th>
+                                <th className='py-2 px-2 font-medium text-black dark:text-white'>
                                     Jabatan
                                 </th>
-                                <th className='py-4 px-4 font-medium text-black dark:text-white'>
-                                    Gaji Pokok
+                                <th className='py-2 px-2 font-medium text-black dark:text-white'>
+                                    Gaji <br /> Pokok
                                 </th>
-                                <th className='py-4 px-4 font-medium text-black dark:text-white'>
-                                    Tunjangan Transport
+                                <th className='py-2 px-2 font-medium text-black dark:text-white'>
+                                    Tunjangan <br />Transport
                                 </th>
-                                <th className='py-4 px-4 font-medium text-black dark:text-white'>
-                                    Uang Makan
+                                <th className='py-2 px-2 font-medium text-black dark:text-white'>
+                                    Uang <br /> Makan
                                 </th>
-                                <th className='py-4 px-4 font-medium text-black dark:text-white'>
-                                    Total
+                                <th className='py-2 px-2 font-medium text-black dark:text-white'>
+                                    Potongan
                                 </th>
-                                <th className='py-4 px-4 font-medium text-black dark:text-white'>
+                                <th className='py-2 px-2 font-medium text-black dark:text-white'>
+                                    Total <br /> Gaji
+                                </th>
+                                <th className='py-2 px-2 font-medium text-black dark:text-white'>
                                     Actions
                                 </th>
                             </tr>
                         </thead>
                         <tbody>
-                            {dataJabatan.map((dataJabatan) => {
+                            {dataGaji.map((dataGaji) => {
                                 return (
-                                    <tr key={dataJabatan.id}>
-                                        <td className='border-b border-[#eee] py-5 px-4 dark:border-strokedark'>
-                                            <p className='text-black dark:text-white'>{dataJabatan.titleJabatan}</p>
+                                    <tr key={dataGaji.id}>
+                                        <td className='border-b border-[#eee] py-5 px-4 text-center dark:border-strokedark'>
+                                            <p className='text-black dark:text-white'>{dataGaji.nik}</p>
                                         </td>
-                                        <td className='border-b border-[#eee] py-5 px-4 dark:border-strokedark'>
-                                            <p className='text-black dark:text-white'>{dataJabatan.gajiPokok}</p>
+                                        <td className='border-b border-[#eee] py-5 px-4 text-center dark:border-strokedark'>
+                                            <p className='text-black dark:text-white'>{dataGaji.namaPegawai}</p>
                                         </td>
-                                        <td className='border-b border-[#eee] py-5 px-4 dark:border-strokedark'>
-                                            <p className='text-black dark:text-white'>{dataJabatan.tunjanganTransport}</p>
+                                        <td className='border-b border-[#eee] py-5 px-4 text-center dark:border-strokedark'>
+                                            <p className='text-black dark:text-white'>{dataGaji.jenisKelamin}</p>
                                         </td>
-                                        <td className='border-b border-[#eee] py-5 px-4 dark:border-strokedark'>
-                                            <p className='text-black dark:text-white'>{dataJabatan.uangMakan}</p>
+                                        <td className='border-b border-[#eee] py-5 px-4 text-center dark:border-strokedark'>
+                                            <p className='text-black dark:text-white'>{dataGaji.titleJabatan}</p>
                                         </td>
-                                        <td className='border-b border-[#eee] py-5 px-4 dark:border-strokedark'>
-                                            <p className='text-black dark:text-white'>{dataJabatan.total}</p>
+                                        <td className='border-b border-[#eee] py-5 px-4 text-center dark:border-strokedark'>
+                                            <p className='text-black dark:text-white'>{dataGaji.gajiPokok}</p>
                                         </td>
-                                        <td className='border-b border-[#eee] py-5 px-4 dark:border-strokedark'>
+                                        <td className='border-b border-[#eee] py-5 px-4 text-center dark:border-strokedark'>
+                                            <p className='text-black dark:text-white'>{dataGaji.tunjanganTransport}</p>
+                                        </td>
+                                        <td className='border-b border-[#eee] py-5 px-4 text-center dark:border-strokedark'>
+                                            <p className='text-black dark:text-white'>{dataGaji.uangMakan}</p>
+                                        </td>
+                                        <td className='border-b border-[#eee] py-5 px-4 text-center dark:border-strokedark'>
+                                            <p className='text-black dark:text-white'>{dataGaji.jumlahPotongan}</p>
+                                        </td>
+                                        <td className='border-b border-[#eee] py-5 px-4 text-center dark:border-strokedark'>
+                                            <p className='text-black dark:text-white'>{dataGaji.totalGaji}</p>
+                                        </td>
+                                        <td className='border-b border-[#eee] py-5 px-4 text-center dark:border-strokedark'>
                                             <div className='flex items-center space-x-3.5'>
                                                 <button className='hover:text-black'>
                                                     <FaRegEdit className="text-primary text-xl hover:text-black dark:hover:text-white" />
@@ -127,7 +223,7 @@ const DataGaji = () => {
                 <div className='flex justify-between items-center mt-4 flex-col md:flex-row md:justify-between'>
                     <div className='flex items-center space-x-2'>
                         <span className='text-gray-5 dark:text-gray-4 text-sm py-4'>
-                            Showing {startIndex}-{endIndex} of {DataJabatanPeople.length} Data Jabatan
+                            Showing {startIndex}-{endIndex} of {DataGajiPegawai.length} Data Gaji Pegawai
                         </span>
                     </div>
                     <div className='flex space-x-2 py-4'>
