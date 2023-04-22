67bbd0d677c2866fb9c0af85164b6a6184cd0a5f
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-04-23 04:10:33 +0700

    Refactor

--- Frontend/src/pages/Pegawai/DashboardPegawai/DashboardPegawai.jsx
+++ Frontend/src/pages/Pegawai/DashboardPegawai/DashboardPegawai.jsx
@@ -8,36 +8,37 @@ const DashboardPegawai = () => {
         <DefaultLayoutPegawai>
             <BreadcrumbPegawai pageName='Dashboard' />
             <div className="mt-6">
-                <h2 className="px-4 py-2 text-black dark:text-white font-medium">
-                    Selamat Datang, Anda Login Sebagai Pegawai
+                <h2 className="px-4 py-2 text-meta-3 font-medium text-center md:text-left">
+                    Selamat Datang, Anda Login Sebagai Pegawai.
                 </h2>
             </div>
-            <div className='py-2 px-6.5 dark:border-strokedark'>
-                <h3 className='font-medium text-black dark:text-white'>
+            <div className="py-2 px-4 md:px-6 dark:border-strokedark text-lg">
+                <h3 className="font-medium text-black dark:text-white text-center md:text-left">
                     Data Pegawai
                 </h3>
             </div>
             <div className="flex flex-col md:flex-row rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark mt-2">
-                <div className="md:w-1/3 drop-shadow-2 w-full px-4 py-4 flex justify-center md:justify-start">
-                    <img className="rounded-xl h-50 w-50 object-cover" src={PegawaiPeople} alt="Gilbert Hutapea" />
+                <div className="md:w-1/3 w-full px-4 py-4 flex justify-center md:justify-start">
+                    <img className="rounded-xl h-80 w-full md:w-80 object-cover" src={PegawaiPeople} alt="Gilbert Hutapea" />
                 </div>
-                <div className="md:w-2/3 px-4 py-6">
-                    <div className="w-full">
-                        <h2 className=" font-medium mb-2.5 block text-black dark:text-white">
-                            Nama Lengkap: Gilbert Hutapea
+                <div className="md:w-2/3 px-4 md:px-20 py-4 md:py-20">
+                    <div className="w-full md:text-lg">
+                        <h2 className="font-medium mb-4 block text-black dark:text-white">
+                            <span className="inline-block w-32 md:w-40">Nama Pegawai</span><span className="inline-block w-7">:</span> Gilbert Hutapea
                         </h2>
-                        <h2 className=" font-medium mb-2.5 block text-black dark:text-white">
-                            Jabatan: FullStack Developer
+                        <h2 className="font-medium mb-4 block text-black dark:text-white">
+                            <span className="inline-block w-32 md:w-40">Jabatan</span><span className="inline-block w-7">:</span> <span className="pl-[-10] md:pl-0">FullStack Developer</span>
                         </h2>
-                        <h2 className=" font-medium mb-2.5 block text-black dark:text-white">
-                            Tanggal Masuk: 15-01-2023
+                        <h2 className="font-medium mb-4 block text-black dark:text-white">
+                            <span className="inline-block w-32 md:w-40">Tanggal Masuk</span><span className="inline-block w-7">:</span> 15-01-2023
                         </h2>
-                        <h2 className=" font-medium mb-2.5 block text-black dark:text-white">
-                            Status: Karyawan Tetap
+                        <h2 className="font-medium mb-4 block text-black dark:text-white">
+                            <span className="inline-block w-32 md:w-40">Status</span><span className="inline-block w-7">:</span><span className="pl-[-8] md:pl-0"> Karyawan Tetap</span>
                         </h2>
                     </div>
                 </div>
             </div>
+
 
         </DefaultLayoutPegawai>
     )
