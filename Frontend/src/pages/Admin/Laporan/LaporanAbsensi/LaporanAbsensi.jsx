16c8a8dc75a4f95e9f520d0b23cad750fb59b85e
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-04-22 23:37:59 +0700

    Refactor

--- Frontend/src/pages/Admin/Laporan/LaporanAbsensi/LaporanAbsensi.jsx
+++ Frontend/src/pages/Admin/Laporan/LaporanAbsensi/LaporanAbsensi.jsx
@@ -1,179 +1,88 @@
-import { useState, useEffect } from 'react';
-import DefaultLayout from '../../../../layout/DefaultLayout';
-import DataSettingPotonganGaji from '../../../../utils/DataSettingPotonganGaji';
+import React from 'react';
 import { Link } from "react-router-dom";
+import DefaultLayout from '../../../../layout/DefaultLayout';
 import Breadcrumb from '../../../../components/Breadcrumb/Breadcrumb';
 import ButtonOne from '../../../../components/Buttons/ButtonOne/ButtonOne';
-import { FaRegEdit, FaPlus } from 'react-icons/fa'
-import { BsTrash3 } from 'react-icons/bs'
-import { BiSearch } from 'react-icons/bi'
-
-const ITEMS_PER_PAGE = 4;
+import { MdOutlineKeyboardArrowDown } from 'react-icons/md'
+import { TfiPrinter } from 'react-icons/tfi'
 
 const LaporanAbsensi = () => {
-    const [currentPage, setCurrentPage] = useState(1);
-    const [startIndex, setStartIndex] = useState(0);
-    const [endIndex, setEndIndex] = useState(ITEMS_PER_PAGE);
-    const [settingPotonganGaji, setSettingPotonganGaji] = useState([]);
-
-    const totalPages = Math.ceil(DataSettingPotonganGaji.length / ITEMS_PER_PAGE);
-
-    useEffect(() => {
-        setSettingPotonganGaji(DataSettingPotonganGaji.slice(startIndex, endIndex));
-    }, [startIndex, endIndex]);
-
-    const goToPrevPage = () => {
-        if (currentPage > 1) {
-            setCurrentPage((prev) => prev - 1);
-            setStartIndex((prev) => prev - ITEMS_PER_PAGE);
-            setEndIndex((prev) => prev - ITEMS_PER_PAGE);
-        }
-    };
-
-    const goToNextPage = () => {
-        if (currentPage < totalPages) {
-            setCurrentPage((prev) => prev + 1);
-            setStartIndex((prev) => prev + ITEMS_PER_PAGE);
-            setEndIndex((prev) => prev + ITEMS_PER_PAGE);
-        }
-    };
-
-
     return (
         <DefaultLayout>
-            <Breadcrumb pageName='Laporan Absensi' />
-            <Link to="/admin/transaksi/setting-potongan-gaji/form-setting-potongan-gaji" >
-                <ButtonOne  >
-                    <span>Tambah Potongan</span>
-                    <span>
-                        <FaPlus />
-                    </span>
-                </ButtonOne>
-            </Link>
-            <div className='rounded-sm border border-stroke bg-white px-5 pt-6 pb-2.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1 mt-6'>
-                <div className="flex justify-between items-center mt-4 flex-col md:flex-row md:justify-between">
-                    <div className="relative flex-2 mb-4 md:mb-0">
-                        <input
-                            type='text'
-                            placeholder='Type to search..'
-                            className='rounded-lg border-[1.5px] border-stroke bg-transparent py-2 pl-10 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary left-0'
-                        />
-                        <span className='absolute left-2 py-3 text-xl'>
-                            <BiSearch />
-                        </span>
-                    </div>
-                </div>
+            <Breadcrumb pageName='Laporan Absensi Pegawai' />
 
-                <div className='max-w-full overflow-x-auto py-4'>
-                    <table className='w-full table-auto'>
-                        <thead>
-                            <tr className='bg-gray-2 text-left dark:bg-meta-4'>
-                                <th className='py-4 px-4 font-medium text-black dark:text-white'>
-                                    Potongan Gaji
-                                </th>
-                                <th className='py-4 px-4 font-medium text-black dark:text-white'>
-                                    Jumlah Potongan
-                                </th>
-                                <th className='py-4 px-4 font-medium text-black dark:text-white'>
-                                    Actions
-                                </th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {settingPotonganGaji.map((settingPotonganGaji) => {
-                                return (
-                                    <tr key={settingPotonganGaji.id}>
-                                        <td className='border-b border-[#eee] py-5 px-4 dark:border-strokedark'>
-                                            <p className='text-black dark:text-white'>{settingPotonganGaji.potonganGaji}</p>
-                                        </td>
-                                        <td className='border-b border-[#eee] py-5 px-4 dark:border-strokedark'>
-                                            <p className='text-black dark:text-white'>{settingPotonganGaji.jumlahPotongan}</p>
-                                        </td>
-                                        <td className='border-b border-[#eee] py-5 px-4 dark:border-strokedark'>
-                                            <div className='flex items-center space-x-3.5'>
-                                                <button className='hover:text-black'>
-                                                    <FaRegEdit className="text-primary text-xl hover:text-black dark:hover:text-white" />
-                                                </button>
-                                                <button className='hover:text-black'>
-                                                    <BsTrash3 className="text-danger text-xl hover:text-black dark:hover:text-white" />
-                                                </button>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                );
-                            })}
-                        </tbody>
-                    </table>
-                </div>
-
-                <div className='flex justify-between items-center mt-4 flex-col md:flex-row md:justify-between'>
-                    <div className='flex items-center space-x-2'>
-                        <span className='text-gray-5 dark:text-gray-4 text-sm py-4'>
-                            Showing {startIndex}-{endIndex} of {DataSettingPotonganGaji.length} Setting Potongan Gaji
-                        </span>
-                    </div>
-                    <div className='flex space-x-2 py-4'>
-                        <button
-                            disabled={currentPage === 1}
-                            onClick={goToPrevPage}
-                            className='py-2 px-6 rounded-lg border border-primary text-primary font-semibold hover:bg-primary hover:text-white dark:text-white dark:border-primary dark:hover:bg-primary dark:hover:text-white disabled:opacity-50'
-                        >
-                            Prev
-                        </button>
-                        {[...Array(Math.min(totalPages, 5))].map((_, i) => {
-                            const page = i + 1;
-                            if (page === currentPage) {
-                                return (
-                                    <div
-                                        key={i}
-                                        className="py-2 px-4 rounded-lg border border-primary bg-primary text-white font-semibold hover:bg-primary dark:text-white dark:bg-primary dark:hover:bg-primary"
-                                    >
-                                        {page}
+            <div className='sm:grid-cols-2'>
+                <div className='flex flex-col gap-9'>
+                    <div className='rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark'>
+                        <div className='border-b border-stroke py-4 px-6.5 dark:border-strokedark'>
+                            <h3 className='font-medium text-black dark:text-white'>
+                                Filter Laporan Absensi Pegawai
+                            </h3>
+                        </div>
+                        <form action='#'>
+                            <div className='p-6.5'>
+                                <div className='mb-4.5 '>
+                                    <div className='w-full mb-4'>
+                                        <label className='mb-2.5 block text-black dark:text-white'>
+                                            Bulan <span className='text-meta-1'>*</span>
+                                        </label>
+                                        <div className='relative z-20 bg-transparent dark:bg-form-input'>
+                                            <select className='relative z-20 w-full appearance-none rounded border border-stroke bg-transparent py-3 px-5 outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary'>
+                                                <option value=''>Pilih Bulan</option>
+                                                <option value=''>Januari</option>
+                                                <option value=''>Februari</option>
+                                                <option value=''>Maret</option>
+                                                <option value=''>April</option>
+                                                <option value=''>Mei</option>
+                                                <option value=''>Juni</option>
+                                                <option value=''>Juli</option>
+                                                <option value=''>Agustus</option>
+                                                <option value=''>September</option>
+                                                <option value=''>Oktober</option>
+                                                <option value=''>November</option>
+                                                <option value=''>Desember</option>
+                                            </select>
+                                            <span className='absolute top-1/2 right-4 z-30 -translate-y-1/2 text-2xl'>
+                                                <MdOutlineKeyboardArrowDown />
+                                            </span>
+                                        </div>
                                     </div>
-                                );
-                            } else if (page === 2 && currentPage > 4) {
-                                return (
-                                    <p
-                                        key={i}
-                                        className="py-2 px-4 border border-gray-2 dark:bg-transparent text-black font-medium bg-gray dark:border-strokedark dark:text-white"
-                                    >
-                                        ...
-                                    </p>
-                                );
-                            } else if (page === totalPages - 1 && currentPage < totalPages - 3) {
-                                return (
-                                    <p
-                                        key={i}
-                                        className="py-2 px-4 border border-gray-2 dark:bg-transparent text-black font-medium bg-gray dark:border-strokedark dark:text-white"
-                                    >
-                                        ...
-                                    </p>
-                                );
-                            } else if (
-                                page === 1 ||
-                                page === totalPages ||
-                                (page >= currentPage - 1 && page <= currentPage + 1)
-                            ) {
-                                return (
-                                    <div
-                                        key={i}
-                                        className="py-2 px-4 rounded-lg border border-gray-2 text-black dark:bg-transparent bg-gray font-medium dark:border-strokedark dark:text-white"
-                                    >
-                                        {page}
-                                    </div>
-                                );
-                            } else {
-                                return null;
-                            }
-                        })}
 
-                        <button
-                            disabled={currentPage === totalPages}
-                            onClick={goToNextPage}
-                            className='py-2 px-6 rounded-lg border border-primary text-primary font-semibold hover:bg-primary hover:text-white dark:text-white dark:border-primary dark:hover:bg-primary dark:hover:text-white disabled:opacity-50'
-                        >
-                            Next
-                        </button>
+                                    <div className='w-full mb-4'>
+                                        <label className='mb-2.5 block text-black dark:text-white'>
+                                            Tahun <span className='text-meta-1'>*</span>
+                                        </label>
+                                        <div className='relative z-20 bg-transparent dark:bg-form-input'>
+                                            <select className='relative z-20 w-full appearance-none rounded border border-stroke bg-transparent py-3 px-5 outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary'>
+                                                <option value=''>Pilih Tahun</option>
+                                                <option value=''>2020</option>
+                                                <option value=''>2021</option>
+                                                <option value=''>2022</option>
+                                                <option value=''>2023</option>
+                                                <option value=''>2024</option>
+                                                <option value=''>2025</option>
+                                                <option value=''>2026</option>
+                                                <option value=''>2027</option>
+                                            </select>
+                                            <span className='absolute top-1/2 right-4 z-30 -translate-y-1/2 text-2xl'>
+                                                <MdOutlineKeyboardArrowDown />
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className='flex flex-col md:flex-row w-full gap-3 text-center'>
+                                    <Link to="" >
+                                        <ButtonOne  >
+                                            <span>Cetak Laporan Absensi</span>
+                                            <span>
+                                                <TfiPrinter />
+                                            </span>
+                                        </ButtonOne>
+                                    </Link>
+                                </div>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
