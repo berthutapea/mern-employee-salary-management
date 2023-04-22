c35afa45915b9f40e6cc266ddc4ebc659d040e3a
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-04-22 23:29:45 +0700

    Refactor

--- Frontend/src/pages/Admin/Laporan/LaporanGaji/LaporanGaji.jsx
+++ Frontend/src/pages/Admin/Laporan/LaporanGaji/LaporanGaji.jsx
@@ -9,14 +9,14 @@ import { TfiPrinter } from 'react-icons/tfi'
 const LaporanGaji = () => {
     return (
         <DefaultLayout>
-            <Breadcrumb pageName='Laporan Gaji' />
-
+            <Breadcrumb pageName='Laporan Gaji Pegawai' />
+            
             <div className='sm:grid-cols-2'>
                 <div className='flex flex-col gap-9'>
                     <div className='rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark'>
                         <div className='border-b border-stroke py-4 px-6.5 dark:border-strokedark'>
                             <h3 className='font-medium text-black dark:text-white'>
-                                Filter Laporan Gaji
+                                Filter Laporan Gaji Pegawai
                             </h3>
                         </div>
                         <form action='#'>
