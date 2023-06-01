8aafc688a9258779ec37a96d215084bce7b68983
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-06-01 22:31:56 +0700

    State Management Login & Register

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,28 +1,28 @@
-// import React, { useState, useEffect } from 'react'
-// import { Routes } from './config';
+import React, { useState, useEffect } from 'react'
+import { Routes } from './config';
 
-// function App() {
-//   const [loading, setLoading] = useState(true)
+function App() {
+  const [loading, setLoading] = useState(true)
 
-//   const preloader = document.getElementById('preloader')
+  const preloader = document.getElementById('preloader')
 
-//   if (preloader) {
-//     setTimeout(() => {
-//       preloader.style.display = 'none'
-//       setLoading(false)
-//     }, 2000);
-//   }
+  if (preloader) {
+    setTimeout(() => {
+      preloader.style.display = 'none'
+      setLoading(false)
+    }, 2000);
+  }
 
-//   useEffect(() => {
-//     setTimeout(() => setLoading(false), 1000)
-//   }, [])
-//   return (
-//     !loading && (
-//       // <Provider store={store}>
-//       <Routes />
-//       // </Provider>
-//     )
-//   )
-// }
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 1000)
+  }, [])
+  return (
+    !loading && (
+      // <Provider store={store}>
+      <Routes />
+      // </Provider>
+    )
+  )
+}
 
-// export default App;
+export default App;
