39b5d2a1a1062d1afafbcce3a3423b0d4d837861
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-07-28 00:59:21 +0700

    Refactor Dashboard Admin

--- Frontend/src/components/atoms/Card/CardThree/index.jsx
+++ Frontend/src/components/atoms/Card/CardThree/index.jsx
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getDataJabatan } from '../../../../config/redux/action';
 import { FaUserTie } from 'react-icons/fa'
 
 const CardThree = () => {
+  const dispatch = useDispatch();
+  const { dataJabatan } = useSelector((state) => state.dataJabatan);
+  const jumlahDataJabatan = dataJabatan.length;
+
+  useEffect(() => {
+    dispatch(getDataJabatan());
+  }, [dispatch]);
+
   return (
     <div className='rounded-sm border border-stroke bg-white py-6 px-7.5 shadow-default dark:border-strokedark dark:bg-boxdark'>
       <div className='flex h-11.5 w-11.5 items-center justify-center rounded-full bg-meta-2 dark:bg-meta-4'>
-      <FaUserTie className="fill-primary dark:fill-white text-xl" />
+        <FaUserTie className="fill-primary dark:fill-white text-xl" />
       </div>
 
       <div className='mt-4 flex items-end justify-between'>
         <div>
           <h4 className='text-title-md font-bold text-black dark:text-white'>
-            5
+            {jumlahDataJabatan}
           </h4>
           <span className='text-sm font-medium'>Data Jabatan</span>
         </div>
--- Frontend/src/components/atoms/Card/CardTwo/index.jsx
+++ Frontend/src/components/atoms/Card/CardTwo/index.jsx
@@ -7,7 +7,6 @@ const CardTwo = () => {
       <div className='flex h-11.5 w-11.5 items-center justify-center rounded-full bg-meta-2 dark:bg-meta-4'>
         <RiAdminFill className="fill-primary dark:fill-white text-xl" />
       </div>
-
       <div className='mt-4 flex items-end justify-between'>
         <div>
           <h4 className='text-title-md font-bold text-black dark:text-white'>
@@ -15,23 +14,6 @@ const CardTwo = () => {
           </h4>
           <span className='text-sm font-medium'>Data Admin</span>
         </div>
-
-        {/* <span className='flex items-center gap-1 text-sm font-medium text-meta-3'>
-          4.35%
-          <svg
-            className='fill-meta-3'
-            width='10'
-            height='11'
-            viewBox='0 0 10 11'
-            fill='none'
-            xmlns='http://www.w3.org/2000/svg'
-          >
-            <path
-              d='M4.35716 2.47737L0.908974 5.82987L5.0443e-07 4.94612L5 0.0848689L10 4.94612L9.09103 5.82987L5.64284 2.47737L5.64284 10.0849L4.35716 10.0849L4.35716 2.47737Z'
-              fill=''
-            />
-          </svg>
-        </span> */}
       </div>
     </div>
   )
