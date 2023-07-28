df6cd10043f5b4e5d5d8b19c53e99d6986bfd7a5
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-07-29 00:05:58 +0700

    Refactor UI Landinge Page

--- Frontend/src/components/molecules/Footer/index.jsx
+++ Frontend/src/components/molecules/Footer/index.jsx
@@ -4,8 +4,8 @@ const Footer = () => {
     return (
         <div>
             <footer className="text-sm text-center px-10 py-6 text-base-100 border-base-300 dark:bg-boxdark-2">
-                <div className="w-full h-[2px] bg-gray-600"></div>
-                <div className="flex flex-col md:flex-row items-center justify-center text-black dark:text-white">
+                <div className="w-full h-[2px] bg-black dark:bg-white"></div>
+                <div className="flex flex-col md:flex-row items-center justify-center mt-5 text-black dark:text-white">
                     <p>&copy; Copyright 2023, PT. Humpus Karbometil Selulosa. All Rights Reserved</p>
                 </div>
             </footer>
