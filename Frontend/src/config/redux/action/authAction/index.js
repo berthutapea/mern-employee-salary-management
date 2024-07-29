4de29ddac5c12823d67340246b75ffbf1ceaeaaa
msnproduction <111676859+berthutapea@users.noreply.github.com>
2024-07-29 21:12:05 +0700

    fix: authActionReducers

--- Frontend/src/config/redux/action/authAction/index.js
+++ Frontend/src/config/redux/action/authAction/index.js
@@ -1,44 +1,44 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
+// import { createAsyncThunk } from "@reduxjs/toolkit";
+// import axios from "axios";
 
-export const loginUser = createAsyncThunk(
-  "user/loginUser",
-  async (user, thunkAPI) => {
-    try {
-      const response = await axios.post('http://localhost:5000/login', {
-        username: user.username,
-        password: user.password
-      });
-      return response.data;
-    } catch (error) {
-      if (error.response) {
-        const message = error.response.data.msg;
-        return thunkAPI.rejectWithValue(message);
-      }
-    }
-  }
-);
+// export const loginUser = createAsyncThunk(
+//   "user/loginUser",
+//   async (user, thunkAPI) => {
+//     try {
+//       const response = await axios.post('http://localhost:5000/login', {
+//         username: user.username,
+//         password: user.password
+//       });
+//       return response.data;
+//     } catch (error) {
+//       if (error.response) {
+//         const message = error.response.data.msg;
+//         return thunkAPI.rejectWithValue(message);
+//       }
+//     }
+//   }
+// );
 
-export const getMe = createAsyncThunk("user/getMe", async (_, thunkAPI) => {
-  try {
-    const response = await axios.get('http://localhost:5000/me');
-    return response.data;
-  } catch (error) {
-    if (error.response) {
-      const message = error.response.data.msg;
-      return thunkAPI.rejectWithValue(message);
-    }
-  }
-});
+// export const getMe = createAsyncThunk("user/getMe", async (_, thunkAPI) => {
+//   try {
+//     const response = await axios.get('http://localhost:5000/me');
+//     return response.data;
+//   } catch (error) {
+//     if (error.response) {
+//       const message = error.response.data.msg;
+//       return thunkAPI.rejectWithValue(message);
+//     }
+//   }
+// });
 
-export const logoutUser = createAsyncThunk("user/logoutUser", async () => {
-  try {
-    const response = await axios.delete("http://localhost:5000/logout");
-    return response.data;
-  } catch (error) {
-    if (error.response) {
-      const message = error.response.data.msg;
-      return thunkAPI.rejectWithValue(message);
-    }
-  }
-});
+// export const logoutUser = createAsyncThunk("user/logoutUser", async () => {
+//   try {
+//     const response = await axios.delete("http://localhost:5000/logout");
+//     return response.data;
+//   } catch (error) {
+//     if (error.response) {
+//       const message = error.response.data.msg;
+//       return thunkAPI.rejectWithValue(message);
+//     }
+//   }
+// });
