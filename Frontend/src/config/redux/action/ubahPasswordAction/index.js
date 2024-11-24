9728a89088d860973e0a007f78d65cde4576bed3
msnproduction <111676859+berthutapea@users.noreply.github.com>
2024-11-24 23:19:10 +0700

    fix: change password action

--- Frontend/src/config/redux/action/ubahPasswordAction/index.js
+++ Frontend/src/config/redux/action/ubahPasswordAction/index.js
@@ -1,41 +1,41 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000';
+// const API_BASE_URL = 'http://localhost:5000';
 
-export const changePasswordRequest = () => ({
-  type: 'CHANGE_PASSWORD_REQUEST',
-});
+// export const changePasswordRequest = () => ({
+//   type: 'CHANGE_PASSWORD_REQUEST',
+// });
 
-export const changePasswordSuccess = (message) => ({
-  type: 'CHANGE_PASSWORD_SUCCESS',
-  payload: message,
-});
+// export const changePasswordSuccess = (message) => ({
+//   type: 'CHANGE_PASSWORD_SUCCESS',
+//   payload: message,
+// });
 
-export const changePasswordFailure = (error) => ({
-  type: 'CHANGE_PASSWORD_FAILURE',
-  payload: error,
-});
+// export const changePasswordFailure = (error) => ({
+//   type: 'CHANGE_PASSWORD_FAILURE',
+//   payload: error,
+// });
 
-export const changePassword = (password, confPassword) => async (dispatch) => {
-  try {
-    dispatch(changePasswordRequest());
+// export const changePassword = (password, confPassword) => async (dispatch) => {
+//   try {
+//     dispatch(changePasswordRequest());
 
-    if (password !== confPassword) {
-      dispatch(changePasswordFailure('Password dan Konfirmasi Password Tidak Cocok'));
-      return;
-    }
+//     if (password !== confPassword) {
+//       dispatch(changePasswordFailure('Password dan Konfirmasi Password Tidak Cocok'));
+//       return;
+//     }
 
-    const response = await axios.patch(`${API_BASE_URL}/change_password`, {
-      password,
-      confPassword,
-    }, {
-      headers: {
-        "Content-type": "application/json",
-      },
-    });
+//     const response = await axios.patch(`${API_BASE_URL}/change_password`, {
+//       password,
+//       confPassword,
+//     }, {
+//       headers: {
+//         "Content-type": "application/json",
+//       },
+//     });
 
-    dispatch(changePasswordSuccess(response.data.msg));
-  } catch (error) {
-    dispatch(changePasswordFailure(error.message));
-  }
-};
+//     dispatch(changePasswordSuccess(response.data.msg));
+//   } catch (error) {
+//     dispatch(changePasswordFailure(error.message));
+//   }
+// };
