11cd1579e626daa0ca86aa13046b1c37a083ba4c
msnproduction <111676859+berthutapea@users.noreply.github.com>
2024-11-25 23:07:31 +0700

    fix: auth reducer

--- Frontend/src/config/redux/reducer/authReducer/index.js
+++ Frontend/src/config/redux/reducer/authReducer/index.js
@@ -1,56 +1,56 @@
-// import { createSlice } from "@reduxjs/toolkit";
-// import { loginUser, getMe } from "../../action/authAction";
+import { createSlice } from "@reduxjs/toolkit";
+import { loginUser, getMe } from "../../action/authAction";
 
-// const initialState = {
-//   user: null,
-//   isError: false,
-//   isSuccess: false,
-//   isLoading: false,
-//   message: "",
-// };
+const initialState = {
+  user: null,
+  isError: false,
+  isSuccess: false,
+  isLoading: false,
+  message: "",
+};
 
-// const authSlice = createSlice({
-//   name: "auth",
-//   initialState,
-//   reducers: {
-//     reset: () => initialState,
-//   },
-//   extraReducers: (builder) => {
-//     builder
-//       .addCase(loginUser.pending, (state) => {
-//         state.isLoading = true;
-//         state.isError = false;
-//         state.isSuccess = false;
-//         state.message = "";
-//       })
-//       .addCase(loginUser.fulfilled, (state, action) => {
-//         state.isLoading = false;
-//         state.isSuccess = true;
-//         state.user = action.payload;
-//       })
-//       .addCase(loginUser.rejected, (state, action) => {
-//         state.isLoading = false;
-//         state.isError = true;
-//         state.message = action.payload;
-//       })
-//       .addCase(getMe.pending, (state) => {
-//         state.isLoading = true;
-//         state.isError = false;
-//         state.isSuccess = false;
-//         state.message = "";
-//       })
-//       .addCase(getMe.fulfilled, (state, action) => {
-//         state.isLoading = false;
-//         state.isSuccess = true;
-//         state.user = action.payload;
-//       })
-//       .addCase(getMe.rejected, (state, action) => {
-//         state.isLoading = false;
-//         state.isError = true;
-//         state.message = action.payload;
-//       });
-//   },
-// });
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    reset: () => initialState,
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(loginUser.pending, (state) => {
+        state.isLoading = true;
+        state.isError = false;
+        state.isSuccess = false;
+        state.message = "";
+      })
+      .addCase(loginUser.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.user = action.payload;
+      })
+      .addCase(loginUser.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+      })
+      .addCase(getMe.pending, (state) => {
+        state.isLoading = true;
+        state.isError = false;
+        state.isSuccess = false;
+        state.message = "";
+      })
+      .addCase(getMe.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.user = action.payload;
+      })
+      .addCase(getMe.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+      });
+  },
+});
 
-// export const { reset } = authSlice.actions;
-// export default authSlice.reducer;
+export const { reset } = authSlice.actions;
+export default authSlice.reducer;
