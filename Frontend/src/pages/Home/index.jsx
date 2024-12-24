a3d27ff9660844d645bed97c2bfdcdd6d7f3d037
msnproduction <111676859+berthutapea@users.noreply.github.com>
2024-12-24 20:45:34 +0700

    fix: home page

--- Frontend/src/pages/Home/index.jsx
+++ Frontend/src/pages/Home/index.jsx
@@ -1,19 +1,19 @@
-import React from "react";
-import { Banner, Navbar } from "../../components";
-import About from "../About";
-import Contact from "../Contact";
+// import React from "react";
+// import { Banner, Navbar } from "../../components";
+// import About from "../About";
+// import Contact from "../Contact";
 
-const Home = () => {
-    return (
-        <>
-            <div className="dark:bg-boxdark">
-                <Navbar />
-                <Banner />
-                <About />
-                <Contact />
-            </div>
-        </>
-    );
-};
+// const Home = () => {
+//     return (
+//         <>
+//             <div className="dark:bg-boxdark">
+//                 <Navbar />
+//                 <Banner />
+//                 <About />
+//                 <Contact />
+//             </div>
+//         </>
+//     );
+// };
 
-export default Home;
+// export default Home;
