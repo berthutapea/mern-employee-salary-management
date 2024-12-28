e07d9406f15e01d8e8c9b3185dc3526960fb9369
msnproduction <111676859+berthutapea@users.noreply.github.com>
2024-12-28 21:29:13 +0700

    fix: spinner

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,28 +1,28 @@
-import React, { useState, useEffect } from 'react'
-import { Routes, store } from './config';
-import { Provider } from 'react-redux';
+// import React, { useState, useEffect } from 'react'
+// import { Routes, store } from './config';
+// import { Provider } from 'react-redux';
 
-function App() {
-  const [loading, setLoading] = useState(true)
-  const preloader = document.getElementById('preloader')
+// function App() {
+//   const [loading, setLoading] = useState(true)
+//   const preloader = document.getElementById('preloader')
 
-  if (preloader) {
-    setTimeout(() => {
-      preloader.style.display = 'none'
-      setLoading(false)
-    }, 2000);
-  }
+//   if (preloader) {
+//     setTimeout(() => {
+//       preloader.style.display = 'none'
+//       setLoading(false)
+//     }, 2000);
+//   }
 
-  useEffect(() => {
-    setTimeout(() => setLoading(false), 1000)
-  }, [])
-  return (
-    !loading && (
-      <Provider store={store}>
-        <Routes />
-      </Provider>
-    )
-  )
-}
+//   useEffect(() => {
+//     setTimeout(() => setLoading(false), 1000)
+//   }, [])
+//   return (
+//     !loading && (
+//       <Provider store={store}>
+//         <Routes />
+//       </Provider>
+//     )
+//   )
+// }
 
-export default App;
+// export default App;
