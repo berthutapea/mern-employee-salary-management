a8ab7ff6500a47800c4cc795dceb69ba2bb4ae5c
msnproduction <111676859+berthutapea@users.noreply.github.com>
2025-01-16 22:03:27 +0700

    fix: footer

--- Frontend/src/components/molecules/Footer/index.jsx
+++ Frontend/src/components/molecules/Footer/index.jsx
@@ -1,15 +1,15 @@
-// import React from "react";
+import React from "react";
 
-// const Footer = () => {
-//     return (
-//         <div>
-//             <footer className="text-sm text-center py-6 text-base-100 border-base-300 dark:bg-boxdark-2">
-//                 <div className="flex flex-col md:flex-row items-center justify-center mt-5 text-black dark:text-white">
-//                     <p>&copy; Copyright 2023, PT. Humpus Karbometil Selulosa. All Rights Reserved</p>
-//                 </div>
-//             </footer>
-//         </div>
-//     );
-// };
+const Footer = () => {
+    return (
+        <div>
+            <footer className="text-sm text-center py-6 text-base-100 border-base-300 dark:bg-boxdark-2">
+                <div className="flex flex-col md:flex-row items-center justify-center mt-5 text-black dark:text-white">
+                    <p>&copy; Copyright 2023, PT. Humpus Karbometil Selulosa. All Rights Reserved</p>
+                </div>
+            </footer>
+        </div>
+    );
+};
 
-// export default Footer;
+export default Footer;
