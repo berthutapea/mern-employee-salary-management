5044c703065fccece1ebe0897e6f6a915ce8ccf1
msnproduction <111676859+berthutapea@users.noreply.github.com>
2025-01-27 15:29:17 +0700

    fix: bot found

--- Frontend/src/components/molecules/NotFound/index.jsx
+++ Frontend/src/components/molecules/NotFound/index.jsx
@@ -1,28 +1,28 @@
-// import React from "react";
-// import { FaHome } from "react-icons/fa";
-// import { Link } from "react-router-dom";
-// import { ButtonThree } from "../../atoms";
+import React from "react";
+import { FaHome } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { ButtonThree } from "../../atoms";
 
-// const NotFound = () => {
-//     return (
-//         <div className="h-screen w-full flex flex-col md:flex-row items-center justify-center">
-//             <h1 className="text-[8rem] font-bold text-primary">404</h1>
-//             <div className="w-24 h-1 md:w-1 md:h-24 bg-primary my-6 md:my-0 md:mx-8"></div>
-//             <div className="flex flex-col items-center">
-//                 <h2 className="text-2xl text-center mb-4">
-//                     Maaf, halaman ini tidak tersedia !
-//                 </h2>
-//                 <Link to="/">
-//                     <ButtonThree>
-//                         <span>Kembali Ke Beranda</span>
-//                         <span>
-//                             <FaHome></FaHome>
-//                         </span>
-//                     </ButtonThree>
-//                 </Link>
-//             </div>
-//         </div>
-//     );
-// };
+const NotFound = () => {
+    return (
+        <div className="h-screen w-full flex flex-col md:flex-row items-center justify-center">
+            <h1 className="text-[8rem] font-bold text-primary">404</h1>
+            <div className="w-24 h-1 md:w-1 md:h-24 bg-primary my-6 md:my-0 md:mx-8"></div>
+            <div className="flex flex-col items-center">
+                <h2 className="text-2xl text-center mb-4">
+                    Maaf, halaman ini tidak tersedia !
+                </h2>
+                <Link to="/">
+                    <ButtonThree>
+                        <span>Kembali Ke Beranda</span>
+                        <span>
+                            <FaHome></FaHome>
+                        </span>
+                    </ButtonThree>
+                </Link>
+            </div>
+        </div>
+    );
+};
 
-// export default NotFound;
+export default NotFound;
