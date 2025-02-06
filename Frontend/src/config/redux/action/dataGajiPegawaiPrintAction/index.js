c9c4a8171312258bb851036d3e0752cf442296ed
Gilbert Hutapea <111676859+berthutapea@users.noreply.github.com>
2025-02-06 23:31:42 +0700

    fix: data salary employee print action

--- Frontend/src/config/redux/action/dataGajiPegawaiPrintAction/index.js
+++ Frontend/src/config/redux/action/dataGajiPegawaiPrintAction/index.js
@@ -1,58 +1,58 @@
-// import axios from "axios";
-// import {
-//     GET_DATA_GAJI_SINGLE_PEGAWAI_SUCCESS,
-//     GET_DATA_GAJI_SINGLE_PEGAWAI_FAILURE,
-// } from "./dataGajiPegawaiPrintActionTypes";
+import axios from "axios";
+import {
+    GET_DATA_GAJI_SINGLE_PEGAWAI_SUCCESS,
+    GET_DATA_GAJI_SINGLE_PEGAWAI_FAILURE,
+} from "./dataGajiPegawaiPrintActionTypes";
 
-// export const viewDataGajiSinglePegawaiSuccess = (data) => ({
-//     type: GET_DATA_GAJI_SINGLE_PEGAWAI_SUCCESS,
-//     payload: data,
-// });
+export const viewDataGajiSinglePegawaiSuccess = (data) => ({
+    type: GET_DATA_GAJI_SINGLE_PEGAWAI_SUCCESS,
+    payload: data,
+});
 
-// export const viewDataGajiSinglePegawaiFailure = (error) => ({
-//     type: GET_DATA_GAJI_SINGLE_PEGAWAI_FAILURE,
-//     payload: error,
-// });
+export const viewDataGajiSinglePegawaiFailure = (error) => ({
+    type: GET_DATA_GAJI_SINGLE_PEGAWAI_FAILURE,
+    payload: error,
+});
 
-// export const viewGajiSinglePegawaiByYear = (dataYear) => async (dispatch) => {
-//     try {
-//         const response = await axios.get(
-//             `http://localhost:5000/data_gaji/month/${dataYear}`
-//         );
-//         const data = response.data;
-//         dispatch(viewDataGajiSinglePegawaiSuccess(data));
-//     } catch (error) {
-//         if (error.response && error.response.data) {
-//             dispatch(viewDataGajiSinglePegawaiFailure("Terjadi kesalahan saat memuat data."));
-//         }
-//     }
-// };
+export const viewGajiSinglePegawaiByYear = (dataYear) => async (dispatch) => {
+    try {
+        const response = await axios.get(
+            `http://localhost:5000/data_gaji/month/${dataYear}`
+        );
+        const data = response.data;
+        dispatch(viewDataGajiSinglePegawaiSuccess(data));
+    } catch (error) {
+        if (error.response && error.response.data) {
+            dispatch(viewDataGajiSinglePegawaiFailure("Terjadi kesalahan saat memuat data."));
+        }
+    }
+};
 
-// export const viewGajiSinglePegawaiByMonth = (dataMonth) => async (dispatch) => {
-//     try {
-//         const response = await axios.get(
-//             `http://localhost:5000/data_gaji/month/${dataMonth}`
-//         );
-//         const data = response.data;
-//         dispatch(viewDataGajiSinglePegawaiSuccess(data));
-//     } catch (error) {
-//         if (error.response && error.response.data) {
-//             dispatch(viewDataGajiSinglePegawaiFailure("Terjadi kesalahan saat memuat data."));
-//         }
-//     }
-// };
+export const viewGajiSinglePegawaiByMonth = (dataMonth) => async (dispatch) => {
+    try {
+        const response = await axios.get(
+            `http://localhost:5000/data_gaji/month/${dataMonth}`
+        );
+        const data = response.data;
+        dispatch(viewDataGajiSinglePegawaiSuccess(data));
+    } catch (error) {
+        if (error.response && error.response.data) {
+            dispatch(viewDataGajiSinglePegawaiFailure("Terjadi kesalahan saat memuat data."));
+        }
+    }
+};
 
-// export const viewGajiSinglePegawaiByName = (nama_pegawai) => async (dispatch) => {
-//     try {
-//         const response = await axios.get(
-//             `http://localhost:5000/data_gaji/name/${nama_pegawai}`
-//         );
-//         const data = response.data;
-//         dispatch(viewDataGajiSinglePegawaiSuccess(data));
-//     } catch (error) {
-//         console.log(error);
-//         if (nama_pegawai) {
-//             dispatch(viewDataGajiSinglePegawaiFailure("Terjadi kesalahan saat memuat data."));
-//         }
-//     }
-// };
+export const viewGajiSinglePegawaiByName = (nama_pegawai) => async (dispatch) => {
+    try {
+        const response = await axios.get(
+            `http://localhost:5000/data_gaji/name/${nama_pegawai}`
+        );
+        const data = response.data;
+        dispatch(viewDataGajiSinglePegawaiSuccess(data));
+    } catch (error) {
+        console.log(error);
+        if (nama_pegawai) {
+            dispatch(viewDataGajiSinglePegawaiFailure("Terjadi kesalahan saat memuat data."));
+        }
+    }
+};
