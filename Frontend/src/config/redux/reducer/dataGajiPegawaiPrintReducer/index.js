5ff592405d8c8637cf6890f343ad7a9f9dbabf1b
berthutapea <berthutapea@gmail.com>
2025-02-19 20:56:45 +0700

    fix: data salary employee print reducer

--- Frontend/src/config/redux/reducer/dataGajiPegawaiPrintReducer/index.js
+++ Frontend/src/config/redux/reducer/dataGajiPegawaiPrintReducer/index.js
@@ -1,31 +1,31 @@
-import {
-    GET_DATA_GAJI_SINGLE_PEGAWAI_SUCCESS,
-    GET_DATA_GAJI_SINGLE_PEGAWAI_FAILURE,
-} from "../../action/dataGajiPegawaiPrintAction/dataGajiPegawaiPrintActionTypes";
+// import {
+//     GET_DATA_GAJI_SINGLE_PEGAWAI_SUCCESS,
+//     GET_DATA_GAJI_SINGLE_PEGAWAI_FAILURE,
+// } from "../../action/dataGajiPegawaiPrintAction/dataGajiPegawaiPrintActionTypes";
 
-const initialState = {
-    dataGajiPegawaiPrint: [], 
-    error: null,
-  };
+// const initialState = {
+//     dataGajiPegawaiPrint: [], 
+//     error: null,
+//   };
   
 
-const dataGajiPegawaiPrintReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case GET_DATA_GAJI_SINGLE_PEGAWAI_SUCCESS:
-            return {
-                ...state,
-                dataGajiPegawaiPrint: action.payload,
-                error: null,
-            };
-        case GET_DATA_GAJI_SINGLE_PEGAWAI_FAILURE:
-            return {
-                ...state,
-                dataGajiPegawaiPrint: null,
-                error: action.payload,
-            };
-        default:
-            return state;
-    }
-};
+// const dataGajiPegawaiPrintReducer = (state = initialState, action) => {
+//     switch (action.type) {
+//         case GET_DATA_GAJI_SINGLE_PEGAWAI_SUCCESS:
+//             return {
+//                 ...state,
+//                 dataGajiPegawaiPrint: action.payload,
+//                 error: null,
+//             };
+//         case GET_DATA_GAJI_SINGLE_PEGAWAI_FAILURE:
+//             return {
+//                 ...state,
+//                 dataGajiPegawaiPrint: null,
+//                 error: action.payload,
+//             };
+//         default:
+//             return state;
+//     }
+// };
 
-export default dataGajiPegawaiPrintReducer;
+// export default dataGajiPegawaiPrintReducer;
