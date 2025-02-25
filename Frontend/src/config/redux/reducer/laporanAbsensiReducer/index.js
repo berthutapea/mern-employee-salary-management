2431f4d579e8097bad35984a23676b71ce7d9588
berthutapea <berthutapea@gmail.com>
2025-02-25 22:14:10 +0700

    fix: absence reducer report

--- Frontend/src/config/redux/reducer/laporanAbsensiReducer/index.js
+++ Frontend/src/config/redux/reducer/laporanAbsensiReducer/index.js
@@ -1,37 +1,37 @@
-import {
-    FETCH_LAPORAN_ABSENSI_SUCCESS,
-    FETCH_LAPORAN_ABSENSI_FAILURE,
-    CLEAR_LAPORAN_ABSENSI,
-} from "../../action/laporanAbsensiAction";
+// import {
+//     FETCH_LAPORAN_ABSENSI_SUCCESS,
+//     FETCH_LAPORAN_ABSENSI_FAILURE,
+//     CLEAR_LAPORAN_ABSENSI,
+// } from "../../action/laporanAbsensiAction";
 
-const initialState = {
-    dataLaporanAbsensi: [],
-    error: null,
-};
+// const initialState = {
+//     dataLaporanAbsensi: [],
+//     error: null,
+// };
 
-const laporanAbsensiReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case FETCH_LAPORAN_ABSENSI_SUCCESS:
-            return {
-                ...state,
-                dataLaporanAbsensi: action.payload,
-                error: null,
-            };
-        case FETCH_LAPORAN_ABSENSI_FAILURE:
-            return {
-                ...state,
-                dataLaporanAbsensi: [],
-                error: action.payload,
-            };
-        case CLEAR_LAPORAN_ABSENSI:
-            return {
-                ...state,
-                dataLaporanAbsensi: [],
-                error: null,
-            };
-        default:
-            return state;
-    }
-};
+// const laporanAbsensiReducer = (state = initialState, action) => {
+//     switch (action.type) {
+//         case FETCH_LAPORAN_ABSENSI_SUCCESS:
+//             return {
+//                 ...state,
+//                 dataLaporanAbsensi: action.payload,
+//                 error: null,
+//             };
+//         case FETCH_LAPORAN_ABSENSI_FAILURE:
+//             return {
+//                 ...state,
+//                 dataLaporanAbsensi: [],
+//                 error: action.payload,
+//             };
+//         case CLEAR_LAPORAN_ABSENSI:
+//             return {
+//                 ...state,
+//                 dataLaporanAbsensi: [],
+//                 error: null,
+//             };
+//         default:
+//             return state;
+//     }
+// };
 
-export default laporanAbsensiReducer;
+// export default laporanAbsensiReducer;
