d60a816b0570dd8f5e968c4512ab8ba3c57f326d
berthutapea <berthutapea@gmail.com>
2025-02-27 22:11:31 +0700

    fix: change password reducer

--- Frontend/src/config/redux/reducer/ubahPasswordReducer/index.js
+++ Frontend/src/config/redux/reducer/ubahPasswordReducer/index.js
@@ -1,33 +1,33 @@
-const initialState = {
-    loading: false,
-    error: null,
-    message: '',
-};
+// const initialState = {
+//     loading: false,
+//     error: null,
+//     message: '',
+// };
 
-const ubahPasswordReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case 'CHANGE_PASSWORD_REQUEST':
-            return {
-                ...state,
-                loading: true,
-                error: null,
-            };
-        case 'CHANGE_PASSWORD_SUCCESS':
-            return {
-                ...state,
-                loading: false,
-                error: null,
-                message: action.payload,
-            };
-        case 'CHANGE_PASSWORD_FAILURE':
-            return {
-                ...state,
-                loading: false,
-                error: action.payload,
-            };
-        default:
-            return state;
-    }
-};
+// const ubahPasswordReducer = (state = initialState, action) => {
+//     switch (action.type) {
+//         case 'CHANGE_PASSWORD_REQUEST':
+//             return {
+//                 ...state,
+//                 loading: true,
+//                 error: null,
+//             };
+//         case 'CHANGE_PASSWORD_SUCCESS':
+//             return {
+//                 ...state,
+//                 loading: false,
+//                 error: null,
+//                 message: action.payload,
+//             };
+//         case 'CHANGE_PASSWORD_FAILURE':
+//             return {
+//                 ...state,
+//                 loading: false,
+//                 error: action.payload,
+//             };
+//         default:
+//             return state;
+//     }
+// };
 
-export default ubahPasswordReducer;
+// export default ubahPasswordReducer;
