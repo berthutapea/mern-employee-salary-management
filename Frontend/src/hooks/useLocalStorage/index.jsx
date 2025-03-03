0b19f805e0e9c17489fd618d26cd52f7f22768fb
berthutapea <berthutapea@gmail.com>
2025-03-03 21:40:36 +0700

    fix: use local storage

--- Frontend/src/hooks/useLocalStorage/index.jsx
+++ Frontend/src/hooks/useLocalStorage/index.jsx
@@ -1,29 +1,29 @@
-// import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
-// const UseLocalStorage = (key, initialValue) => {
-//   const [storedValue, setStoredValue] = useState(() => {
-//     try {
-//       const item = window.localStorage.getItem(key)
-//       return item ? JSON.parse(item) : initialValue
-//     } catch (error) {
-//       console.log(error)
-//       return initialValue
-//     }
-//   })
+const UseLocalStorage = (key, initialValue) => {
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      const item = window.localStorage.getItem(key)
+      return item ? JSON.parse(item) : initialValue
+    } catch (error) {
+      console.log(error)
+      return initialValue
+    }
+  })
 
-//   useEffect(() => {
-//     try {
-//       const valueToStore =
-//         typeof storedValue === 'function'
-//           ? storedValue(storedValue)
-//           : storedValue
-//       window.localStorage.setItem(key, JSON.stringify(valueToStore))
-//     } catch (error) {
-//       console.log(error)
-//     }
-//   }, [key, storedValue])
+  useEffect(() => {
+    try {
+      const valueToStore =
+        typeof storedValue === 'function'
+          ? storedValue(storedValue)
+          : storedValue
+      window.localStorage.setItem(key, JSON.stringify(valueToStore))
+    } catch (error) {
+      console.log(error)
+    }
+  }, [key, storedValue])
 
-//   return [storedValue, setStoredValue]
-// }
+  return [storedValue, setStoredValue]
+}
 
-// export default UseLocalStorage;
+export default UseLocalStorage;
