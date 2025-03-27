396e05dbb4ff1bdc4ca06e0a3fa9a641d90564bb
berthutapea <berthutapea@gmail.com>
2025-03-27 19:25:22 +0700

    fix: button three

--- Frontend/src/components/atoms/Buttons/ButtonThree/index.jsx
+++ Frontend/src/components/atoms/Buttons/ButtonThree/index.jsx
@@ -1,11 +1,11 @@
-// import React from "react";
+import React from "react";
 
-// const ButtonThree = ({ children, className, onClick }) => {
-//     return (
-//         <button className={`${className} inline-flex items-center justify-center gap-2 rounded-md bg-primary py-2 px-10 text-center font-medium text-white hover:bg-opacity-90 lg:px-8 xl:px-10`} onClick={onClick}>
-//             {children}
-//         </button>
-//     );
-// };
+const ButtonThree = ({ children, className, onClick }) => {
+    return (
+        <button className={`${className} inline-flex items-center justify-center gap-2 rounded-md bg-primary py-2 px-10 text-center font-medium text-white hover:bg-opacity-90 lg:px-8 xl:px-10`} onClick={onClick}>
+            {children}
+        </button>
+    );
+};
 
-// export default ButtonThree;
+export default ButtonThree;
