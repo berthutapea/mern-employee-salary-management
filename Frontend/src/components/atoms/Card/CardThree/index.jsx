b0b4c3578dc7678e19499406ef10820de46b352d
berthutapea <berthutapea@gmail.com>
2025-04-01 22:52:07 +0700

    card three

--- Frontend/src/components/atoms/Card/CardThree/index.jsx
+++ Frontend/src/components/atoms/Card/CardThree/index.jsx
@@ -1,33 +1,33 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { getDataJabatan } from '../../../../config/redux/action';
-import { FaUserTie } from 'react-icons/fa'
+// import React, { useEffect } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { getDataJabatan } from '../../../../config/redux/action';
+// import { FaUserTie } from 'react-icons/fa'
 
-const CardThree = () => {
-  const dispatch = useDispatch();
-  const { dataJabatan } = useSelector((state) => state.dataJabatan);
-  const jumlahDataJabatan = dataJabatan.length;
+// const CardThree = () => {
+//   const dispatch = useDispatch();
+//   const { dataJabatan } = useSelector((state) => state.dataJabatan);
+//   const jumlahDataJabatan = dataJabatan.length;
 
-  useEffect(() => {
-    dispatch(getDataJabatan());
-  }, [dispatch]);
+//   useEffect(() => {
+//     dispatch(getDataJabatan());
+//   }, [dispatch]);
 
-  return (
-    <div className='rounded-sm border border-stroke bg-white py-6 px-7.5 shadow-default dark:border-strokedark dark:bg-boxdark'>
-      <div className='flex h-11.5 w-11.5 items-center justify-center rounded-full bg-meta-2 dark:bg-meta-4'>
-        <FaUserTie className="fill-primary dark:fill-white text-xl" />
-      </div>
+//   return (
+//     <div className='rounded-sm border border-stroke bg-white py-6 px-7.5 shadow-default dark:border-strokedark dark:bg-boxdark'>
+//       <div className='flex h-11.5 w-11.5 items-center justify-center rounded-full bg-meta-2 dark:bg-meta-4'>
+//         <FaUserTie className="fill-primary dark:fill-white text-xl" />
+//       </div>
 
-      <div className='mt-4 flex items-end justify-between'>
-        <div>
-          <h4 className='text-title-md font-bold text-black dark:text-white'>
-            {jumlahDataJabatan}
-          </h4>
-          <span className='text-sm font-medium'>Data Jabatan</span>
-        </div>
-      </div>
-    </div>
-  )
-}
+//       <div className='mt-4 flex items-end justify-between'>
+//         <div>
+//           <h4 className='text-title-md font-bold text-black dark:text-white'>
+//             {jumlahDataJabatan}
+//           </h4>
+//           <span className='text-sm font-medium'>Data Jabatan</span>
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
 
-export default CardThree;
+// export default CardThree;
